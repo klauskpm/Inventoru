0ee1d1f07aab97d2ae086a01ad553f22293e6f87
Klaus Kazlauskas <klausmachado@gmail.com>
2016-11-30 22:40:59 -0200

    Product Provider
     - Query

---
+++ app/src/main/java/br/com/klauskpm/inventory/data/ProductProvider.java
@@ -0,0 +1,93 @@
+package br.com.klauskpm.inventory.data;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+import br.com.klauskpm.inventory.data.ProductContract.ProductEntry;
+
+/**
+ * Created by Kazlauskas on 30/11/2016.
+ */
+
+public class ProductProvider extends ContentProvider {
+    private static final int PRODUCTS = 100;
+    private static final int PRODUCT_ID = 101;
+
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    public static final String LOG_TAG = ProductProvider.class.getSimpleName();
+
+    static {
+        sUriMatcher.addURI(ProductContract.CONTENT_AUTHORITY, ProductContract.PATH_PRODUCTS,
+                PRODUCTS);
+
+        sUriMatcher.addURI(ProductContract.CONTENT_AUTHORITY, ProductContract.PATH_PRODUCTS + "/#",
+                PRODUCT_ID);
+    }
+
+    private InventoryDbHelper mInventoryDbHelper;
+
+    @Override
+    public boolean onCreate() {
+        mInventoryDbHelper = new InventoryDbHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+        SQLiteDatabase database = mInventoryDbHelper.getReadableDatabase();
+
+        Cursor cursor;
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PRODUCTS:
+                cursor = database.query(ProductEntry.TABLE_NAME, projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+
+            case PRODUCT_ID:
+                selection = ProductEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+
+                cursor = database.query(ProductEntry.TABLE_NAME, projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Cannot query URI " + uri);
+        }
+
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        return 0;
+    }
+}
