eb1b711cb2a67e9e2ad42058009422465e140681
Klaus Kazlauskas <klausmachado@gmail.com>
2016-11-30 22:11:06 -0200

    Product Contract

---
+++ app/src/main/java/br/com/klauskpm/inventory/data/ProductContract.java
@@ -0,0 +1,36 @@
+package br.com.klauskpm.inventory.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by Kazlauskas on 30/11/2016.
+ */
+
+public final class ProductContract {
+
+    private ProductContract() {}
+
+    public static final String CONTENT_AUTHORITY = "br.com.klauskpm.inventory";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PRODUCTS = "products";
+
+    public static final class ProductEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PRODUCTS);
+        public static final String CONTENT_LIST = CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
+
+        public static final String CONTENT_LIST_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/"
+                + CONTENT_LIST;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/"
+                + CONTENT_LIST;
+
+        public static final String TABLE_NAME = "products";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PRODUCT_NAME = "name";
+        public static final String COLUMN_PRODUCT_QUANTITY = "quantity";
+        public static final String COLUMN_PRODUCT_PRICE = "price";
+        public static final String COLUMN_PRODUCT_IMAGE = "image";
+    }
+}
