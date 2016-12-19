0c8a10ac713867ca073184a13f6b3ca8343e3dd5
Klaus Kazlauskas <klausmachado@gmail.com>
2016-12-19 10:49:49 -0200

    Detail Activity

---
+++ app/src/main/java/br/com/klauskpm/inventory/DetailActivity.java
@@ -0,0 +1,21 @@
+package br.com.klauskpm.inventory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+}
