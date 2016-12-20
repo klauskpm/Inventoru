c9e482b874f44ef2ac745813ca046987303a20bf
Klaus Kazlauskas <klausmachado@gmail.com>
2016-12-20 11:11:00 -0200

    Detail activity loading product basic data

--- app/src/main/java/br/com/klauskpm/inventory/DetailActivity.java
+++ app/src/main/java/br/com/klauskpm/inventory/DetailActivity.java
@@ -1,15 +1,56 @@
 package br.com.klauskpm.inventory;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class DetailActivity extends AppCompatActivity {
+import br.com.klauskpm.inventory.data.ProductContract.ProductEntry;
+
+public class DetailActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+
+    private static final int DETAIL_PRODUCT_LOADER = 1;
+
+    public static final int NEW_PRODUCT_TITLE = R.string.new_product_activity_title;
+    public static final int UPDATE_PRODUCT_TITLE = R.string.update_product_activity_title;
+
+    private Uri mProductUri;
+
+    private EditText mTitleEditText;
+    private Button mIncrementQuantityButton;
+    private Button mDecrementQuantityButton;
+    private TextView mQuantityTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        mProductUri = intent.getData();
+        int actionTitle = NEW_PRODUCT_TITLE;
+
+        if (mProductUri != null) {
+            actionTitle = UPDATE_PRODUCT_TITLE;
+            getLoaderManager().initLoader(DETAIL_PRODUCT_LOADER, null, this);
+        } else {
+            invalidateOptionsMenu();
+        }
+
+        setTitle(getString(actionTitle));
+
+        mTitleEditText = (EditText) findViewById(R.id.title);
+        mIncrementQuantityButton = (Button) findViewById(R.id.increment_product_quantity);
+        mDecrementQuantityButton = (Button) findViewById(R.id.decrement_product_quantity);
+        mQuantityTextView = (TextView) findViewById(R.id.quantity);
     }
 
     @Override
@@ -17,5 +58,50 @@ public class DetailActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_detail, menu);
         return true;
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle bundle) {
+        switch (id) {
+            case DETAIL_PRODUCT_LOADER:
+                String[] projection = {
+                        ProductEntry._ID,
+                        ProductEntry.COLUMN_PRODUCT_TITLE,
+                        ProductEntry.COLUMN_PRODUCT_PRICE,
+                        ProductEntry.COLUMN_PRODUCT_QUANTITY,
+                        ProductEntry.COLUMN_PRODUCT_IMAGE
+                };
+
+                return new CursorLoader(
+                        this,
+                        mProductUri,
+                        projection,
+                        null,
+                        null,
+                        null
+                );
+
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        if (cursor.moveToFirst()) {
+            int columnTitleIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_TITLE);
+            int columnPriceIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_PRICE);
+            int columnQuantityIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_QUANTITY);
+            int columnImageIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_IMAGE);
+
+            mTitleEditText.setText(cursor.getString(columnTitleIndex));
+            mQuantityTextView.setText(cursor.getInt(columnQuantityIndex) + "");
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mTitleEditText.setText("");
+        mQuantityTextView.setText("0");
     }
 }
